d8d40146a5b75878308898df3ded207c50c54c2d
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-03 07:05:29 -0500

    [ADD]: User Entity

---
+++ src/main/java/com/alex/excel/users/entity/User.java
@@ -0,0 +1,29 @@
+package com.alex.excel.users.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+    private String country;
+    private String telephone;
+
+}
