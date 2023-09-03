b98cff0a686a2ea0db1be6953acd460ca957a5ed
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-03 07:00:25 -0500

    [ADD]: connect to db

---
+++ src/test/java/com/alex/excel/ExcelToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelToDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
