4237687365b89e31f3be75d114ccd580251f1a92
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-03 08:47:11 -0500

    [ADD]: UserController (findAll with paging)

--- src/main/java/com/alex/excel/users/controller/UserController.java
+++ src/main/java/com/alex/excel/users/controller/UserController.java
@@ -1,13 +1,15 @@
 package com.alex.excel.users.controller;
 
+import com.alex.excel.common.constants.PaginationConstants;
+import com.alex.excel.users.dto.PaginatedUsersResponseDto;
 import com.alex.excel.users.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
@@ -27,6 +29,20 @@ public class UserController {
         userService.massiveUserCreationFromFile(file);
 
         return new ResponseEntity<>(null, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<PaginatedUsersResponseDto> findAll(
+            @RequestParam(defaultValue = PaginationConstants.DEFAULT_PAGE) int page,
+            @RequestParam(defaultValue = PaginationConstants.DEFAULT_SIZE) int size,
+            @RequestParam(defaultValue = PaginationConstants.DEFAULT_SORT_BY) String sortBy,
+            @RequestParam(defaultValue = PaginationConstants.DEFAULT_SORT_DIR) String sortDir
+    ) {
+        Sort.Direction direction = Sort.Direction.fromString(sortDir);
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return ResponseEntity.ok(userService.findAll(pageable));
     }
 
 }
